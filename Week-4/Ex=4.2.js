fa6481e8733750a048dc8e0766e76cb385f0f245
Sushrut Gabhane <sushgabhane123@gmail.com>
2022-08-23 21:08:11 +0530

    Ex=4.2

---
+++ Week-4/Ex=4.2.js
@@ -0,0 +1,27 @@
+<!-- https://js-dk14ox.stackblitz.io -->
+
+
+var Person = function() {};
+
+Person.prototype.initialize = function(name, age)
+{
+  this.name = name;
+  this.age = age;
+}
+Person.prototype.describe = function()
+{
+    return this.name + ", " + this.age;
+}
+
+var Teacher = function() {};
+Teacher.prototype = new Person();
+
+Teacher.prototype.learn = function(subject)
+{
+    console.log(this.name + " teach " + subject);
+}
+
+var him = new Teacher();
+
+him.initialize("Adam", 45);
+him.learn("Inheritance");
